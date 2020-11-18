0fd9bd013471526d8c9c901048c4aed4ed13c4df
ericrr <robertoeric@hotmail.com>
2020-11-18 11:36:39 -0300

    update security

--- security.tf
+++ security.tf
@@ -41,37 +41,37 @@ resource "aws_security_group" "sg-jumpbox-qa" {
 
 
 
-resource "aws_security_group" "sg-jumpbox-dev" {
-  name        = "pes-dev-${var.servico}-${var.ambiente}"
-  description = "sg-pes-dev-${var.servico}-${var.ambiente}"
-  vpc_id      = data.aws_vpc.vpc_private.id
+# resource "aws_security_group" "sg-jumpbox-dev" {
+#   name        = "pes-dev-${var.servico}-${var.ambiente}"
+#   description = "sg-pes-dev-${var.servico}-${var.ambiente}"
+#   vpc_id      = data.aws_vpc.vpc_private.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks       = ["0.0.0.0/0"]
-    description = "Acesso SSH "
-  }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks       = ["0.0.0.0/0"]
+#     description = "Acesso SSH "
+#   }
 
 
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port       = 0
+#     to_port         = 0
+#     protocol        = "-1"
+#     cidr_blocks     = ["0.0.0.0/0"]
+#   }
 
-    tags = {
-        Name =  "pes-dev-${var.servico}-${var.ambiente}"
-        # Product = "${var.product}"
-        # Environment = "${var.environment}"
-        # Cost = "${var.customer}"
-        # Customer = "${var.customer}"      
-        Deploy = "${var.deploy}"
-        # QAStop = "${var.qastop}"
-    }
+#     tags = {
+#         Name =  "pes-dev-${var.servico}-${var.ambiente}"
+#         # Product = "${var.product}"
+#         # Environment = "${var.environment}"
+#         # Cost = "${var.customer}"
+#         # Customer = "${var.customer}"      
+#         Deploy = "${var.deploy}"
+#         # QAStop = "${var.qastop}"
+#     }
 
-}
+# }
 
 
