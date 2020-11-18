67d01d55d9a10c6932ec2112a304ad715309edfb
ericrr <robertoeric@hotmail.com>
2020-11-18 16:15:24 -0300

    update

---
+++ security-dev.tf
@@ -0,0 +1,42 @@
+
+resource "aws_security_group" "sg-jumpbox-dev" {
+  name        = "pes-dev-${var.servico}-${var.ambiente}"
+  description = "sg-pes-dev-${var.servico}-${var.ambiente}"
+  vpc_id      = data.aws_vpc.vpc_private.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+    description = "Acesso SSH "
+  }
+
+  ingress {
+    from_port   = 5432  
+    to_port     = 5432
+    protocol    = "tcp"
+    self = true
+    description = "Acesso RDS"    
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+    tags = {
+        Name =  "pes-dev-${var.servico}-${var.ambiente}"
+        # Product = "${var.product}"
+        # Environment = "${var.environment}"
+        # Cost = "${var.customer}"
+        # Customer = "${var.customer}"      
+        Deploy = "${var.deploy}"
+        # QAStop = "${var.qastop}"
+    }
+
+}
+
+
